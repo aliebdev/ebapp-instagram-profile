ccacbe44e21264d4ae2dfbcda8e10faf988a40e0
Ali Ebadi <ebadialideveloper@gmail.com>
2022-07-18 14:19:33 +0430

    Models : highlight.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -13,14 +13,4 @@ final List<String> posts = [
   "assets/22-01.jpg",
 ];
 
-final List<String> highlights = [
-  "assets/Reels-01.jpg",
-  "assets/2x Speed-01.jpg",
-  "assets/Tips-01.png",
-  "assets/Packages-01.jpg",
-  "assets/Quotes.jpg",
-  "assets/Nemone kar-01.jpg",
-  "assets/Learn-01.jpg",
-];
-
 const String profileImage = "assets/aliebadi_logo.jpg";
