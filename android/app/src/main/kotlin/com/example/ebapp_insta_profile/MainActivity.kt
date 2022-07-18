c8124b3395e5e5715ec2480cf39a77e63987fd1b
Ali Ebadi <ebadialideveloper@gmail.com>
2022-07-18 13:23:52 +0430

    first commit

---
+++ android/app/src/main/kotlin/com/example/ebapp_insta_profile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ebapp_insta_profile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,26 @@
+final List<String> posts = [
+  "assets/28-01.jpg",
+  "assets/29-01.jpg",
+  "assets/30-01.jpg",
+  "assets/20-01.jpg",
+  "assets/17-01.jpg",
+  "assets/22-01.jpg",
+  "assets/28-01.jpg",
+  "assets/29-01.jpg",
+  "assets/30-01.jpg",
+  "assets/20-01.jpg",
+  "assets/17-01.jpg",
+  "assets/22-01.jpg",
+];
+
+final List<String> highlights = [
+  "assets/Reels-01.jpg",
+  "assets/2x Speed-01.jpg",
+  "assets/Tips-01.png",
+  "assets/Packages-01.jpg",
+  "assets/Quotes.jpg",
+  "assets/Nemone kar-01.jpg",
+  "assets/Learn-01.jpg",
+];
+
+const String profileImage = "assets/aliebadi_logo.jpg";
